64f467a785a62740db8974c65cf0e44cce7f1cc7
Unknown <bogdyich@gmail.com>
2017-11-01 17:09:24 +0000

    Added func strcpy()

--- Homework #4/4.1.c
+++ Homework #4/4.1.c
@@ -8,7 +8,18 @@ int strlen(char *s) {
 	return n;
 }
 
+void strcpy(char *dst, char *src) {
+	int i = 0;
+	while ((*(dst + i) = *(src + i)) != '\0') {
+		i++;
+	}
+}
+
 int main() {
+	char a[5] = "hasd\0";
+	char b[5] = "yqwe\0";
+	strcpy(a, b);
 	printf("%d\n", strlen("Hello, world!\0"));
+	printf("%s\n", a);
 	return 0;
 }
