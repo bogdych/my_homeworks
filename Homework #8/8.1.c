bc520a90e82ce566582a9f2a58433c9112fa1c6a
Unknown <bogdyich@gmail.com>
2017-12-26 23:28:17 +0000

    Added checking of allocated memory and removed magic numbers

--- Homework #8/8.1.c
+++ Homework #8/8.1.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+const double limitTime = 20;
+
 void swap(int *a, int *b) {
     *a = *a ^ *b;
     *b = *a ^ *b;
@@ -75,55 +77,69 @@ void main() {
     clock_t startTime;
     clock_t endTime;
     double operatingTime;
+    double prevOperatingTime = 0;
     int aSize[] = {5, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
     printf("\n\n\n");
     printf("\t+---------------+-------------+--------------+-----------+\n");
     printf("\t|Size of array  |    O(n^2)   | O(n * log n) |   O(n)    |\n");
     printf("\t+---------------+-------------+--------------+-----------+\n");
+    int lastElem = aSize[sizeof(aSize)/sizeof(aSize[0]) - 1];
+    int lengthOfLastElem = 0;
+    while (lastElem) {
+        lengthOfLastElem++;
+        lastElem = lastElem / 10;
+    }
     for (int ind = 0; ind < sizeof(aSize) / sizeof(int); ind++) {
         int *a1 = (int *)calloc(aSize[ind], sizeof(int));
         int *a2 = (int *)calloc(aSize[ind], sizeof(int));
         int *a3 = (int *)calloc(aSize[ind], sizeof(int));
-        int r;
-        for (int i = 0; i < aSize[ind]; i++) {
-            r = rand() % (1 << 15);
-            a1[i] = r;
-            a2[i] = r;
-            a3[i] = r;
-        }
-        printf("\t|%d", aSize[ind]);
-        for (int i = 0; i < 9 - ind; i++) {
-           printf(" ");
-        }     
-        printf("\t| ");  
+        if (a1 && a2 && a3) {
+            int r;
+            for (int i = 0; i < aSize[ind]; i++) {
+                r = rand() % (1 << 15);
+                a1[i] = r;
+                a2[i] = r;
+                a3[i] = r;
+            }
+            printf("\t|%d", aSize[ind]);
+            for (int i = 0; i < lengthOfLastElem - ind; i++) {
+               printf(" ");
+            }     
+            printf("\t| ");  
 
-        if (ind < 6) {
+            if (prevOperatingTime > limitTime) {
+                startTime = clock();
+                selectionSort(a1, aSize[ind]);
+                endTime = clock();
+                operatingTime = ((double)(endTime - startTime)) / CLOCKS_PER_SEC;
+                printf("%10.6f  |  ", operatingTime);
+                prevOperatingTime = operatingTime;
+            }
+            else {
+                printf("   n/a      |  ");
+            }
+
             startTime = clock();
-            selectionSort(a1, aSize[ind]);
+            quickSort(a2, 0, aSize[ind] - 1);
             endTime = clock();
             operatingTime = ((double)(endTime - startTime)) / CLOCKS_PER_SEC;
-            printf("%10.6f  |  ", operatingTime);
-        }
+            printf("%10.6f  | ", operatingTime);
+
+            startTime = clock();
+            countSort(a3, aSize[ind ]);
+            endTime = clock();
+            operatingTime = ((double)(endTime - startTime)) / CLOCKS_PER_SEC;
+            printf("%9.6f |\n", operatingTime);
+
+            printf("\t+---------------+-------------+--------------+-----------+\n");
+            free(a1);
+            free(a2);
+            free(a3);
+        } 
         else {
-            printf("   n/a      |  ");
+            printf("Couldn't allocate memory\n");
+            exit(0);
         }
-
-        startTime = clock();
-        quickSort(a2, 0, aSize[ind] - 1);
-        endTime = clock();
-        operatingTime = ((double)(endTime - startTime)) / CLOCKS_PER_SEC;
-        printf("%10.6f  | ", operatingTime);
-        
-        startTime = clock();
-        countSort(a3, aSize[ind ]);
-        endTime = clock();
-        operatingTime = ((double)(endTime - startTime)) / CLOCKS_PER_SEC;
-        printf("%9.6f |\n", operatingTime);
-
-        printf("\t+---------------+-------------+--------------+-----------+\n");
-        free(a1);
-        free(a2);
-        free(a3);
     }
     printf("\n\n");
 }
